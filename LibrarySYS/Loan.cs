50eaedb232871f78e454218fa2dd2e3c1eb076b2
Heta Hokkanen <heta.hokkanen@gmail.com>
2025-11-14 14:50:13 +0000

    Added classes

---
+++ LibrarySYS/Loan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySYS
+{
+    class Loan
+    {
+        private string LoanID;
+        private string MemberID;
+        private string BookID;
+        private DateTime DateReserved;
+    }
+}
