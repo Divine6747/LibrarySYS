bab2e5715a5c0af45d9ab76a6804a41981bd563c
Heta Hokkanen <heta.hokkanen@gmail.com>
2025-11-14 14:53:35 +0000

    Added classes, Member, Book and Loan

---
+++ LibrarySYS/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySYS
+{
+    class Book
+    {
+        private string BookID;
+        private bool ISNB;
+        private string Title;
+        private string Author;
+        private string Genre;
+        private string Description;
+    }
+}
