c497eafb9346085760b7bbe9bb847b54da806242
Heta Hokkanen <t00276584@mymtu.ie>
2025-11-19 16:54:00 +0000

    Interface membermanagement added

---
+++ LibrarySYS/IMemberManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySYS
+{
+    internal interface IMemberManager
+    {
+        void AddMember(Member member);
+        void UpdateMember(Member member);
+        void DeRegisterMember(string memberID);
+        Member FindMemberByID(string memberID);
+        List<Member> GetAllMembers();
+
+    }
+}
