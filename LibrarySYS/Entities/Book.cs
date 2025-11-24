b6e7d22e95a8aec07b7deb859650dfcc08cd1f13
StudentHetaHokkanen( <StudentHetaHokkanen(@R206-KY-JG10WX3>
2025-11-24 17:26:23 +0000

    Added enum genre and fixed genre setters and getters accordingly

--- LibrarySYS/Entities/Book.cs
+++ LibrarySYS/Entities/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibrarySYS.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +11,17 @@ namespace LibrarySYS
     {
             // test add
         private static List<Book> books = new List<Book>();
+
         private string BookID;
-        private bool ISBN;
+        private string ISBN;
         private string Title;
         private string Author;
         private DateTime Publication;
-        private string Genre;
+        List<Genre> Genre;
         private string Description;
 
 
-        public Book(string bookID, Boolean isbn, string title, string author, DateTime publication, string genre, string description)
+        public Book(string bookID, string isbn, string title, string author, DateTime publication, List<Genre> genre, string description)
         {
             this.BookID = bookID;
             this.ISBN = isbn;
@@ -33,20 +35,20 @@ namespace LibrarySYS
 
 
         public string GetBookID() { return BookID; }
-        public bool GetISBN() { return ISBN; }
+        public string GetISBN() { return ISBN; }
         public string GetTitle() { return Title; }
         public string GetAuthor() { return Author; }
         public DateTime GetPublication() { return Publication; }
-        public string GetGenre() { return Genre; }
+        public List<Genre> GetGenre() { return Genre; }
         public string GetDescription() { return Description; }
 
 
         public void setBookID (string bookID) { this.BookID = bookID; }
-        public void SetISBN(bool isbn) { this.ISBN = isbn; }
+        public void SetISBN(string isbn) { this.ISBN = isbn; }
         public void SetTitle(string title) { this.Title = title; }
         public void SetAuthor(string author) { this.Author = author; }
         public void SetPublication(DateTime publication) { this.Publication = publication; }
-        public void SetGenre(string genre) { this.Genre = genre; }
+        public void SetGenre(List<Genre> genre) { this.Genre = genre; }
         public void SetDescription(string description) { this.Description = description; }
 
         public void AddBook(Book book)
---
+++ LibrarySYS/Enum/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySYS.Enum
+{
+    enum Genre
+    {
+        Action,
+        Adventure,
+        Comedy,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        SciFi,
+        NonFiction
+    }
+}
