aabb08a003bc4072b0ed494639eac11f5c8bed90
swpv <swpv@email.cz>
2025-11-24 16:26:36 +0000

    Adding IbookManger class

---
+++ LibrarySYS/Interfaces/IbookManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySYS
+{
+    interface IbookManager
+    {
+        void AddBook(Book book);
+        void Update(Book book);
+        void RemoveBook(Book book);
+        void FindBookById(Book book);
+        List<Book> SearchByTitle(String tile);
+
+        List<Book> GetAllBooks();
+
+
+
+
+
+
+    }
+}
