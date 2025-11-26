dd6c7eeae60b8c2a575d7807c21883d3c7d0aaa8
Divine6747 <118355466+Divine6747@users.noreply.github.com>
2025-11-26 16:21:36 +0000

    Add Idgerator

---
+++ LibrarySYS/Generator/IdGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySYS.Helpers
+{
+    public static class IdGenerator
+    {
+        private static int _memberCounter = 0;
+        private static int _bookCounter = 0;
+        private static int _reservationCounter = 0;
+        private static int _loanCounter = 0;
+
+        public static string GenerateMemberId()
+        {
+            _memberCounter++;
+            return "M" + _memberCounter.ToString("D4");
+        }
+
+        public static string GenerateBookId()
+        {
+            _bookCounter++;
+            return "B" + _bookCounter.ToString("D4");
+        }
+
+        public static string GenerateReservationId()
+        {
+            _reservationCounter++;
+            return "R" + _reservationCounter.ToString("D4");
+        }
+
+        public static string GenerateLoanId()
+        {
+            _loanCounter++;
+            return "L" + _loanCounter.ToString("D4");
+        }
+    }
+}
